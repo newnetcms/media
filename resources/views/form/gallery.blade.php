513378d8748a84f5fc58425888cd6a30e0664d8b
quang dat <datnquit@gmail.com>
2023-08-28 23:50:54 +0700

    fix gallery

--- resources/views/form/gallery.blade.php
+++ resources/views/form/gallery.blade.php
@@ -3,7 +3,8 @@
     <div class="col-12">
         <div class="group-validate gallery-form-group @error(get_dot_array_form($name)) is-invalid @enderror">
             <div class="gallery-list gallery-list-{{$name}}">
-                @if(($listMedia = object_get($item, get_dot_array_form($name))) && $listMedia instanceof \Illuminate\Support\Collection && is_numeric($listMedia->first()))
+                <input type="hidden" name="{{ $name }}">
+            @if(($listMedia = object_get($item, get_dot_array_form($name))) && $listMedia instanceof \Illuminate\Support\Collection && is_numeric($listMedia->first()))
                     @foreach($listMedia as $mediaId)
                         @if($media = get_media($mediaId))
                             <div class="gallery-item">
